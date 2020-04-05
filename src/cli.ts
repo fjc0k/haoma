f5ce8e7dab8287319c07711d74da511e1345a2e8
Jay Fong <fjc0kb@gmail.com>
2020-04-06 01:39:31 +0800

    feat(cli): 依赖安装

--- src/cli.ts
+++ src/cli.ts
@@ -1,13 +1,18 @@
 #!/usr/bin/env node
+import spawn from 'cross-spawn'
 import yargs from 'yargs'
 import { dedent } from 'vtils'
-import { existsSync, writeFileSync } from 'fs'
+import { existsSync, readFileSync, writeFileSync } from 'fs'
 import { join } from 'path'
+import { PackageJson } from 'type-fest'
 
 yargs
   .usage('Usage: $0 <command> [options]')
   .help('help', 'Show help')
   .alias('help', 'h')
+  // Show help if no args
+  // ref: https://github.com/yargs/yargs/issues/895
+  .demandCommand(1, '')
   .command<{ override: boolean }>(
     'init',
     'Initialize the config files',
@@ -23,91 +28,158 @@ yargs
       const cwd = process.cwd()
       const packageName: string = require('../package.json').name
 
-      const writeable = (path: string, writer: (path: string) => unknown) => {
-        const existing = existsSync(path)
-        if (!existing || (existing && argv.override)) {
-          writer(path)
-          console.log(`✔️ Write ${path}`)
+      // ========= 文件写入 =========
+      {
+        console.log('Write config files...')
+
+        const writeable = (path: string, writer: (path: string) => any) => {
+          const existing = existsSync(path)
+          if (!existing || (existing && argv.override)) {
+            writer(path)
+            console.log(`✔️ Write ${path}`)
+          }
         }
+
+        // .editorconfig
+        writeable(join(cwd, '.editorconfig'), path => {
+          writeFileSync(
+            path,
+            `${dedent`
+              # http://editorconfig.org
+              root = true
+
+              [*]
+              indent_style = space
+              indent_size = 2
+              end_of_line = lf
+              charset = utf-8
+              trim_trailing_whitespace = true
+              insert_final_newline = true
+
+              # The JSON files contain newlines inconsistently
+              [*.json]
+              insert_final_newline = ignore
+
+              # Minified JavaScript files shouldn't be changed
+              [**.min.js]
+              indent_style = ignore
+              insert_final_newline = ignore
+
+              [*.md]
+              trim_trailing_whitespace = false
+            `}\n`,
+          )
+        })
+
+        // .eslintrc.js
+        writeable(join(cwd, '.eslintrc.js'), path => {
+          writeFileSync(
+            path,
+            `${dedent`
+              /** @type import('${packageName}').ESLintConfig */
+              module.exports = require('${packageName}').getESLintConfig()
+            `}\n`,
+          )
+        })
+
+        // .eslintignore
+        writeable(join(cwd, '.eslintignore'), path => {
+          writeFileSync(
+            path,
+            `${dedent`
+              !**/.*
+              node_modules
+              lib
+              dist
+            `}\n`,
+          )
+        })
+
+        // .prettierrc.js
+        writeable(join(cwd, '.prettierrc.js'), path => {
+          writeFileSync(
+            path,
+            `${dedent`
+              /** @type import('${packageName}').PrettierConfig */
+              module.exports = require('${packageName}').getPrettierConfig()
+            `}\n`,
+          )
+        })
+
+        // .prettierignore
+        writeable(join(cwd, '.prettierignore'), path => {
+          writeFileSync(
+            path,
+            `${dedent`
+              package-lock.json
+              node_modules
+              lib
+              dist
+            `}\n`,
+          )
+        })
+
+        console.log('✔️ Write config files')
       }
 
-      // .editorconfig
-      writeable(join(cwd, '.editorconfig'), path => {
-        writeFileSync(
-          path,
-          dedent`
-            # http://editorconfig.org
-            root = true
+      // ========= 依赖安装 =========
+      {
+        console.log('Install dependencies...')
 
-            [*]
-            indent_style = space
-            indent_size = 2
-            end_of_line = lf
-            charset = utf-8
-            trim_trailing_whitespace = true
-            insert_final_newline = true
-
-            # The JSON files contain newlines inconsistently
-            [*.json]
-            insert_final_newline = ignore
-
-            # Minified JavaScript files shouldn't be changed
-            [**.min.js]
-            indent_style = ignore
-            insert_final_newline = ignore
-
-            [*.md]
-            trim_trailing_whitespace = false
-          `,
+        const currentPackageJson = join(cwd, 'package.json')
+        const currentPackageInfo: PackageJson = existsSync(currentPackageJson)
+          ? JSON.parse(readFileSync(currentPackageJson).toString())
+          : {}
+        const currentPackageDeps = new Set(
+          Object.keys({
+            ...currentPackageInfo.dependencies,
+            ...currentPackageInfo.devDependencies,
+          }),
         )
-      })
 
-      // .eslintrc.js
-      writeable(join(cwd, '.eslintrc.js'), path => {
-        writeFileSync(
-          path,
-          dedent`
-            /** @type import('${packageName}').ESLintConfig */
-            module.exports = require('${packageName}').getESLintConfig()
-          `,
-        )
-      })
+        const needInstallPackages = [
+          packageName,
+          'eslint',
+          'prettier',
+          'husky',
+          'lint-staged',
+        ].filter(name => !currentPackageDeps.has(name))
 
-      // .eslintignore
-      writeable(join(cwd, '.eslintignore'), path => {
-        writeFileSync(
-          path,
-          dedent`
-            !**/.*
-            node_modules
-            lib
-            dist
-          `,
-        )
-      })
+        if (needInstallPackages.length > 0) {
+          const useYarn = existsSync(join(cwd, 'yarn.lock'))
+          if (useYarn) {
+            spawn.sync('yarn', ['add', ...needInstallPackages, '-D'], {
+              stdio: 'inherit',
+            })
+          } else {
+            spawn.sync('npm', ['i', ...needInstallPackages, '-D'], {
+              stdio: 'inherit',
+            })
+          }
+        }
 
-      // .prettierrc.js
-      writeable(join(cwd, '.prettierrc.js'), path => {
-        writeFileSync(
-          path,
-          dedent`
-            /** @type import('${packageName}').PrettierConfig */
-            module.exports = require('${packageName}').getPrettierConfig()
-          `,
-        )
-      })
+        if (!currentPackageInfo.husky && !currentPackageInfo['lint-staged']) {
+          const currentPackageInfo = JSON.parse(
+            readFileSync(currentPackageJson).toString(),
+          )
+          currentPackageInfo.husky = {
+            hooks: {
+              'pre-commit': 'lint-staged',
+            },
+          }
+          currentPackageInfo['lint-staged'] = {
+            '*.{css,less,scss,sass,html,htm,vue,yml,yaml,json,md}': [
+              'prettier --write',
+            ],
+            '*.{js,jsx,ts,tsx}': ['eslint --fix', 'prettier --write'],
+          }
+          writeFileSync(
+            currentPackageJson,
+            JSON.stringify(currentPackageInfo, null, 2),
+          )
+        }
 
-      // .prettierignore
-      writeable(join(cwd, '.prettierignore'), path => {
-        writeFileSync(
-          path,
-          dedent`
-            package-lock.json
-            node_modules
-            lib
-            dist
-          `,
-        )
-      })
+        console.log('✔️ Install dependencies')
+      }
     },
   ).argv
