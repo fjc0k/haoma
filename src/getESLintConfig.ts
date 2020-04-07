87a6e195f8f3e94ff0e211cb3c20b77aec73655c
Jay Fong <fjc0kb@gmail.com>
2020-04-07 13:29:56 +0800

    feat: 添加 reactPragma, reactVersion 配置项

--- src/getESLintConfig.ts
+++ src/getESLintConfig.ts
@@ -1,4 +1,5 @@
 import { Linter } from 'eslint'
+import { LiteralUnion } from 'vtils'
 
 export interface GetESLintConfigPayload {
   /**
@@ -7,10 +8,26 @@ export interface GetESLintConfigPayload {
    * @default process.cwd()
    */
   projectRoot?: string
+
+  /**
+   * React 标识符。
+   *
+   * @default 'React'
+   */
+  reactPragma?: string
+
+  /**
+   * React 版本。默认自动检测。
+   *
+   * @default 'detect'
+   */
+  reactVersion?: LiteralUnion<'detect', string>
 }
 
 export function getESLintConfig({
   projectRoot = process.cwd(),
+  reactPragma = 'React',
+  reactVersion = 'detect',
 }: GetESLintConfigPayload = {}): Linter.Config {
   // This is a workaround for https://github.com/eslint/eslint/issues/3458
   require('@rushstack/eslint-config/patch-eslint6')
@@ -20,5 +37,11 @@ export function getESLintConfig({
   return {
     root: true,
     extends: [require.resolve('./ESLintConfig')],
+    settings: {
+      react: {
+        pragma: reactPragma,
+        version: reactVersion,
+      },
+    },
   }
 }
