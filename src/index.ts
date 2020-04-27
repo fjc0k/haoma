52b7595b9466ca29ebad298674b5cd8c514bc114
Jay Fong <fjc0kb@gmail.com>
2020-04-27 17:55:33 +0800

    fix: 导出 jestTypes

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
-// @index('./{get*,types.ts}', f => `export * from '${f.path}'`)
+// @index('./{get*,types.ts,jestTypes.ts}', f => `export * from '${f.path}'`)
 export * from './getESLintConfig'
 export * from './getJestConfig'
 export * from './getPrettierConfig'
+export * from './jestTypes'
 export * from './types'
 // @endindex
---
+++ src/jestTypes.ts
@@ -0,0 +1,7 @@
+import 'jest-chain'
+
+import 'jest-extended'
+
+import '@testing-library/jest-dom'
+
+import 'snapshot-diff'
