58f60a8e644f300b50543849ecbc85e074a18d18
Jay Fong <fjc0kb@gmail.com>
2020-04-27 19:36:40 +0800

    fix: 查找应用根目录

--- src/generateTypes.ts
+++ src/generateTypes.ts
@@ -1,33 +1,37 @@
+import pkgDir from 'pkg-dir'
 import { existsSync, mkdirSync, writeFileSync } from 'fs'
 import { join } from 'path'
 import { PackageJson } from 'type-fest'
 
-const cwd = process.cwd()
-const nodeModulesPath = join(cwd, 'node_modules')
-const typesPath = join(nodeModulesPath, '@types')
-const generatedHaomaTypesPath = join(typesPath, 'generated__haoma')
+const appRoot = pkgDir.sync(join(__dirname, '../..'))
 
-for (const path of [nodeModulesPath, typesPath, generatedHaomaTypesPath]) {
-  if (!existsSync(path)) {
-    mkdirSync(path)
+if (appRoot) {
+  const nodeModulesPath = join(appRoot, 'node_modules')
+  const typesPath = join(nodeModulesPath, '@types')
+  const generatedHaomaTypesPath = join(typesPath, 'generated__haoma')
+
+  for (const path of [nodeModulesPath, typesPath, generatedHaomaTypesPath]) {
+    if (!existsSync(path)) {
+      mkdirSync(path)
+    }
   }
+
+  writeFileSync(
+    join(generatedHaomaTypesPath, 'index.d.ts'),
+    `import 'haoma/lib/jestSetup'`,
+  )
+
+  writeFileSync(
+    join(generatedHaomaTypesPath, 'package.json'),
+    JSON.stringify(
+      {
+        name: '@types/generated__haoma',
+        version: '0.0.0',
+        main: '',
+        types: 'index.d.ts',
+      } as PackageJson,
+      null,
+      2,
+    ),
+  )
 }
-
-writeFileSync(
-  join(generatedHaomaTypesPath, 'index.d.ts'),
-  `import 'haoma/lib/jestSetup'`,
-)
-
-writeFileSync(
-  join(generatedHaomaTypesPath, 'package.json'),
-  JSON.stringify(
-    {
-      name: '@types/generated__haoma',
-      version: '0.0.0',
-      main: '',
-      types: 'index.d.ts',
-    } as PackageJson,
-    null,
-    2,
-  ),
-)
