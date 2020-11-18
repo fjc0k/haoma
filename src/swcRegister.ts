1b076c208a83cd32137e9aaed2a8942c985f5c45
Jay Fong <fjc0kb@gmail.com>
2020-11-18 09:16:01 +0800

    fix: swc

--- src/swcRegister.ts
+++ src/swcRegister.ts
@@ -1,11 +1,9 @@
 import { TsConfigJson } from 'vtils/types'
 
 require('@swc-node/register/lib/register').register({
-  compilerOptions: {
-    target: 'es2019',
-    module: 'commonjs',
-    moduleResolution: 'node',
-    esModuleInterop: true,
-    sourceMap: true,
-  },
-} as TsConfigJson)
+  target: 'es2019',
+  module: 'commonjs',
+  moduleResolution: 'node',
+  esModuleInterop: true,
+  sourceMap: false,
+} as TsConfigJson['compilerOptions'])
