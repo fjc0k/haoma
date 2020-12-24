553fd734cd46f75bfd9910af62bf82d716cca758
Jay Fong <fjc0kb@gmail.com>
2020-12-24 14:36:03 +0800

    chore: tweaks

--- haoma-compile.config.ts
+++ haoma-compile.config.ts
@@ -2,34 +2,34 @@ import { basename } from 'path'
 import { getCompileConfig } from './lib/index'
 
 export default getCompileConfig([
-  {
-    name: 'cjs',
-    inputFiles: ['src/*.ts', '!**/*.test.*'],
-    outDir: 'lib2',
-    module: 'cjs',
-    rollupDts: true,
-    rollupDtsFiles: ['index.d.ts'],
-    rollupDtsIncludedPackages: ['vtils'],
-  },
-  {
-    name: 'esm',
-    inputFiles: ['src/index.ts'],
-    outDir: 'lib3',
-    module: 'esm',
-    target: 'node',
-    renameImport: [
-      {
-        original: /\.\/getJestConfig/,
-        replacement: 'llllll',
-      },
-    ],
-  },
+  // {
+  //   name: 'cjs',
+  //   inputFiles: ['src/*.ts', '!**/*.test.*'],
+  //   outDir: 'lib2',
+  //   module: 'cjs',
+  //   rollupDts: true,
+  //   rollupDtsFiles: ['index.d.ts'],
+  //   rollupDtsIncludedPackages: ['vtils'],
+  // },
+  // {
+  //   name: 'esm',
+  //   inputFiles: ['src/index.ts'],
+  //   outDir: 'lib3',
+  //   module: 'esm',
+  //   target: 'node',
+  //   renameImport: [
+  //     {
+  //       original: /\.\/getJestConfig/,
+  //       replacement: 'llllll',
+  //     },
+  //   ],
+  // },
   {
     name: 'esm',
     inputFiles: ['tests/x.tsx'],
     outDir: 'lib4',
     module: 'esm',
-    target: 'node',
+    target: 'browser',
     getCssModulesScopedName({ className, fileName }) {
       return `${basename(fileName).split('.')[0]}_${className}`
     },
--- tests/x.tsx
+++ tests/x.tsx
@@ -1,87 +1,97 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
-import './fn'
-import './xx.less'
-import './xx2.scss'
-import React, { Component, FC, useState } from 'react'
+// import './fn'
+// import './xx.less'
+// import './xx2.scss'
+// import React, { Component, FC, useState } from 'react'
 
-// @ts-ignore
-import _ from './xx3.less'
-
-console.log(_)
-
-export default class X extends Component {
-  static yyy = 3
-
-  static y2yy = 3
-
-  static ff() {
-    console.log(1)
+export class Xxx {
+  constructor(private id: number) {
+    ;(this as any).x = id
   }
 
-  xd = 1
-
-  get ppp() {
-    return 1
-  }
-
-  constructor(props: any) {
-    super(props)
-  }
-
-  x() {}
-
-  mounted() {
-    class Cc {}
-    console.log(new Cc())
-    try {
-      const x = 1
-      console.log(x === 1)
-      console.log(x && String(x))
-    } catch (e) {}
-  }
-
-  // dd
-  /** ddkk */
-  handleFF() {}
-
-  renderTTd() {}
-
-  renderTT() {}
-
-  render() {
-    return (
-      <div>
-        <span />
-        <div />
-        <span id='dd' onClick={() => {}}>
-          hello
-          {[1, 2].map(x => (
-            <div key={x} className='dd' id='dd'>
-              {x}
-            </div>
-          ))}
-          <div />
-        </span>
-        <div>hello</div>
-      </div>
-    )
+  d() {
+    console.log(this.id)
   }
 }
 
-export function Test() {
-  const r = useState()
-  return r
-}
+// // @ts-ignore
+// import _ from './xx3.less'
 
-export const BButton: FC = () => <div />
+// console.log(_)
 
-BButton.displayName = 'BButton'
+// export default class X extends Component {
+//   static yyy = 3
 
-switch (1) {
-  case 1:
-    const x = 1
-    console.log(x)
-    break
-  default:
-    break
-}
+//   static y2yy = 3
+
+//   static ff() {
+//     console.log(1)
+//   }
+
+//   xd = 1
+
+//   get ppp() {
+//     return 1
+//   }
+
+//   constructor(props: any) {
+//     super(props)
+//   }
+
+//   x() {}
+
+//   mounted() {
+//     class Cc {}
+//     console.log(new Cc())
+//     try {
+//       const x = 1
+//       console.log(x === 1)
+//       console.log(x && String(x))
+//     } catch (e) {}
+//   }
+
+//   // dd
+//   /** ddkk */
+//   handleFF() {}
+
+//   renderTTd() {}
+
+//   renderTT() {}
+
+//   render() {
+//     return (
+//       <div>
+//         <span />
+//         <div />
+//         <span id='dd' onClick={() => {}}>
+//           hello
+//           {[1, 2].map(x => (
+//             <div key={x} className='dd' id='dd'>
+//               {x}
+//             </div>
+//           ))}
+//           <div />
+//         </span>
+//         <div>hello</div>
+//       </div>
+//     )
+//   }
+// }
+
+// export function Test() {
+//   const r = useState()
+//   return r
+// }
+
+// export const BButton: FC = () => <div />
+
+// BButton.displayName = 'BButton'
+
+// switch (1) {
+//   case 1:
+//     const x = 1
+//     console.log(x)
+//     break
+//   default:
+//     break
+// }
